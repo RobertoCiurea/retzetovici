153feaa91dac3e6aaad398c824b2e42a94288d6b
RobertoCiurea <robertociureaionut@gmail.com>
2024-12-21 16:58:52 +0200

    added and protected routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('/my-account', function(){
+        return view('my-account')->name('my-account');
+    });
+});
+Route::middleware(['alreadyLoggedIn'])->group(function(){
+    Route::get('/login',[LoginController::class, "create"])->name('login');
+    
+    Route::get('/register',[RegisterController::class, "create"])->name('register');
+
+});
+
+
+Route::post('/register', [RegisterController::class, "store"]);
+Route::post('/login', [LoginController::class, "login"]);
