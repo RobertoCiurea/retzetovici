ca682d53d9d6ab3da3cd316c6e9be3a62b6aad53
RobertoCiurea <robertociureaionut@gmail.com>
2024-12-21 16:57:51 +0200

    created register view

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <div class="flex flex-col gap-10 justify-center items-center pt-20 font-quicksand">
+        
+        <h1 class="text-xl md:text-3xl">Înregistrează-te</h1>
+        <x-auth-form type='register' action='/register'></x-auth-form>
+    </div>
+</x-layout>
