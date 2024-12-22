f5ece3b0b26f88ac185d4c70b5c4e2ec81d1d7e2
RobertoCiurea <robertociureaionut@gmail.com>
2024-12-22 19:21:11 +0200

    added categories and popular recipes sections

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,7 @@
 <x-layout>
 <x-glider></x-glider>
+    <!--categories component-->
+    <x-categories></x-categories>
+    <x-popular-recipes></x-popular-recipes>
     <h1>Hello world</h1>
 </x-layout>
