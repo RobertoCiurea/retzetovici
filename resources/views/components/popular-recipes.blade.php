1c8576b50d6a127fa561fe3a1d37cd1e46dfa6fc
RobertoCiurea <robertociureaionut@gmail.com>
2024-12-22 19:22:51 +0200

    added popular recipes view

---
+++ resources/views/components/popular-recipes.blade.php
@@ -0,0 +1,6 @@
+<div class="flex flex-col items-center mt-10" id="popular-recipes">
+    <h1 class="text-2xl md:text-4xl text-accent font-kanit font-bold mb-10 px-5">Rețete populare</h1>
+    <x-recipe-card image="{{url('/images/slider/slider-image-1.png')}}" title='Placinta cu branza si carnati' category='Retete internationale' duration={{60}} likes={{24}} views={{126}}></x-recipe-card>
+    
+
+</div>
