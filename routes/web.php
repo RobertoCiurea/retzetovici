0c5dc92e18019ade4ca25c6999f1518ba1e5a2d2
RobertoCiurea <robertociureaionut@gmail.com>
2024-12-22 19:22:24 +0200

    added my-account and logout logic

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,33 @@
 <?php
 
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\RegisterController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogOutController;
+use App\Http\Controllers\RegisterController;
 
 Route::get('/', function () {
     return view('home');
 });
 
 Route::middleware(['auth'])->group(function(){
-    Route::get('/my-account', function(){
-        return view('my-account')->name('my-account');
+    Route::get('/my-account', function(Request $request){
+        $content = $request->get('content');
+        return view('my-account', ["content"=>$content]);
+    })->name('my-account');
+    Route::get('/account-details', function(){
+        return redirect('my-account?content=account-details');
+    });
+    Route::get('/my-recipes', function(){
+        //get user's recipes from db
+        return redirect('my-account?content=my-recipes');
+    });
+    Route::get('/saved-recipes', function(){
+        //get user's saved recipes from db
+        return redirect('my-account?content=saved-recipes');
     });
 });
+
 Route::middleware(['alreadyLoggedIn'])->group(function(){
     Route::get('/login',[LoginController::class, "create"])->name('login');
     
@@ -23,3 +38,4 @@ Route::middleware(['alreadyLoggedIn'])->group(function(){
 
 Route::post('/register', [RegisterController::class, "store"]);
 Route::post('/login', [LoginController::class, "login"]);
+Route::post('/logout', [LogOutController::class, "logout"]);
