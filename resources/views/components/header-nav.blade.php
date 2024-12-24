c2722487ac6a2b01d866c96397771135b163f19a
RobertoCiurea <robertociureaionut@gmail.com>
2024-12-24 19:52:28 +0200

    modified header

--- resources/views/components/header-nav.blade.php
+++ resources/views/components/header-nav.blade.php
@@ -8,15 +8,19 @@
             <span class="w-0 group-hover:w-full block bg-background h-[2px] shadow-xl transition-all duration-500"></span>
         </li>
         <li class="text-xl text-background drop-shadow-xl flex flex-col group">
-            <a href="#"> Explorează rețete populare</a>
+            <button type="button" id="popular-recipes-button">
+                Explorează rețete populare
+                </button> 
+            <span class="w-0 group-hover:w-full block bg-background h-[2px] shadow-xl transition-all duration-500"></span>
+            </li>
+        <li class="text-xl cursor-pointer text-background drop-shadow-xl flex flex-col group">
+           <button type="button" id="categories-button">
+            Categorii
+           </button>
             <span class="w-0 group-hover:w-full block bg-background h-[2px] shadow-xl transition-all duration-500"></span>
             </li>
         <li class="text-xl text-background drop-shadow-xl flex flex-col group">
-            <a href="#"> Categorii</a>
-            <span class="w-0 group-hover:w-full block bg-background h-[2px] shadow-xl transition-all duration-500"></span>
-            </li>
-        <li class="text-xl text-background drop-shadow-xl flex flex-col group">
-            <form action="/my-account" method="GET">
+            <form action="/account-details" method="GET">
                 <button type="submit">Contul meu</button>
                 <span class="w-0 group-hover:w-full block bg-background h-[2px] shadow-xl transition-all duration-500"></span>
             </form>
