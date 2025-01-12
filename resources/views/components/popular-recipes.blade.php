455176a303fe2cddf087318200fadd859915c0ef
RobertoCiurea <robertociureaionut@gmail.com>
2025-01-12 19:13:42 +0200

    render eaxch recipe card

--- resources/views/components/popular-recipes.blade.php
+++ resources/views/components/popular-recipes.blade.php
@@ -1,6 +1,27 @@
-<div class="flex flex-col items-center mt-10" id="popular-recipes">
+<div class="flex flex-col items-center mt-10 px-5 " id="popular-recipes">
+
+ 
     <h1 class="text-2xl md:text-4xl text-accent font-kanit font-bold mb-10 px-5">Rețete populare</h1>
-    <x-recipe-card image="{{url('/images/slider/slider-image-1.png')}}" title='Placinta cu branza si carnati' category='Retete internationale' duration={{60}} likes={{24}} views={{126}}></x-recipe-card>
-    
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-20">
+
+        
+        <!--Format category like tags-->
+        @foreach ($recipes as $recipe)
+        <x-recipe-card 
+        :image="$recipe->image" 
+        :title="$recipe->title" 
+        :category="$recipe->category"
+        :difficulty="$recipe->difficulty"
+        :duration="$recipe->duration"
+        :likes="$recipe->likes"
+        :views="$recipe->views"
+        :username="$recipe->username"
+        :date="$recipe->created_at->format('d m 20y')"
+        :ingredients="$recipe->ingredients"
+        :tags="$recipe->tags"
+        />
+        
+        @endforeach
+    </div>
 
 </div>
