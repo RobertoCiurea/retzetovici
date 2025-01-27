7f2bf726c14ab71403cce767e2ff867c845c2ab4
RobertoCiurea <robertociureaionut@gmail.com>
2025-01-27 20:29:14 +0200

    completed model logic for comments, likes and saves

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Recipe extends Model
 {
+    use HasFactory;
     protected $fillable=[
         'title',
         'description',
@@ -28,4 +30,27 @@ class Recipe extends Model
         'steps'=>'array',
         'tags'=>'array'
     ];
+    
+    public function comments(){
+        return $this->hasMany(Comment::class, 'recipe_id');
+    }
+    public function likes()
+{
+    return $this->hasMany(Like::class, 'recipe_id');
+}
+
+public function saves(){
+    return $this->hasMany(SavedRecipe::class, 'recipe_id');
+}
+
+
+public function isLikedByUser($userId)
+{
+    return $this->likes()->where('user_id', $userId)->exists();
+}
+public function isSavedByUser($userId)
+{
+    return $this->saves()->where('user_id', $userId)->exists();
+}
+
 }
