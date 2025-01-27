9cc9aa8186e041ea7316a0a1ae0217f211280404
RobertoCiurea <robertociureaionut@gmail.com>
2025-01-27 20:33:39 +0200

    changed dynamic data to user from database and not from Auth facade

--- resources/views/components/account-details.blade.php
+++ resources/views/components/account-details.blade.php
@@ -16,13 +16,13 @@
                 <span class="text-sm text-red-600">{{$message}}</span>
             @enderror
                 <div class="flex gap-3">
-                    <span id="name" class="text-lg italic">{{auth()->user()->name}}</span>
+                    <span id="name" class="text-lg italic">{{$user->name}}</span>
                     <button type="button" id="edit-name" class="bg-green-700 hover:bg-green-800 transition-colors px-2 py-1 rounded-lg text-white shadow-xl">Editeaza</button>
                 </div>
-                <form action="{{ route('user.update', ['userId' => auth()->user()->id]) }}" id="name-form" method="POST" class="hidden">
+                <form action="{{ route('user.update', ['userId' => $user->id]) }}" id="name-form" method="POST" class="hidden">
                     @csrf
                     @method("PUT")
-                    <input type="text" name="name" class="px-2 py-1 bg-white shadow-xl rounded-md" value="{{auth()->user()->name}}" >
+                    <input type="text" name="name" class="px-2 py-1 bg-white shadow-xl rounded-md" value="{{$user->name}}" >
                     <button type="submit" class="bg-green-700 hover:bg-green-800 transition-colors px-2 py-1 rounded-lg text-white shadow-xl">Confirma</button>
                 </form>
             </div>
@@ -34,19 +34,19 @@
                     <span class="text-sm text-red-600">{{$message}}</span>
                 @enderror
                 <div class="flex gap-3">
-                    <span id="email" class="text-lg italic" >{{auth()->user()->email}}</span>
+                    <span id="email" class="text-lg italic" >{{$user->email}}</span>
                     <button type="button" id="edit-email" class="bg-green-700 hover:bg-green-800 transition-colors px-2 py-1 rounded-lg text-white shadow-xl">Editeaza</button>
                 </div>
-                <form action="{{ route('user.update', ['userId' => auth()->user()->id]) }}" id="email-form" method="POST" class="hidden">
+                <form action="{{ route('user.update', ['userId' => $user->id]) }}" id="email-form" method="POST" class="hidden">
                     @csrf
                     @method("PUT")
-                    <input type="text" name="email" class="px-2 py-1 bg-white shadow-xl rounded-md" value="{{auth()->user()->email}}" >
+                    <input type="text" name="email" class="px-2 py-1 bg-white shadow-xl rounded-md" value="{{$user->email}}" >
                     <button type="submit" class="bg-green-700 hover:bg-green-800 transition-colors px-2 py-1 rounded-lg text-white shadow-xl">Confirma</button>
                 </form>
             </div>
 
             <!--Delete account section-->
-            <form action="{{ route('user.delete', ['userId' => auth()->user()->id]) }}" method="POST">
+            <form action="{{ route('user.delete', ['userId' => $user->id]) }}" method="POST">
                 @csrf
                 @method("DELETE")
                 <button type="submit" class="bg-red-600 hover:bg-red-700 text-white font-quicksand px-3 py-1 rounded-lg text-lg">Sterge cont</button>
@@ -56,9 +56,9 @@
         <div class="flex flex-col items-start gap-20 text-black">
             <h1 class="font-kanit text-2xl md:text-3xl ">Statisticile contului</h1>
             <!--Recipes counter-->
-            <span class="md:text-xl font-semibold">Numar retete: 0</span>
-            <span class="md:text-xl font-semibold">Aprecieri: 0</span>
-            <span class="md:text-xl font-semibold">Retete salvate: 0</span>
+            <span class="md:text-xl font-semibold">Numar retete: {{$user->recipes_counter}}</span>
+            <span class="md:text-xl font-semibold">Aprecieri: {{$user->likes}}</span>
+            <span class="md:text-xl font-semibold">Retete salvate: {{$user->saved_recipes}}</span>
         </div>
 </div>
 
