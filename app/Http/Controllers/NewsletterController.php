512fa7c3431922ad03cbd259430f77055606e015
RobertoCiurea <robertociureaionut@gmail.com>
2025-02-11 20:26:05 +0200

    added newsetter route logic

---
+++ app/Http/Controllers/NewsletterController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Newsletter;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+
+class NewsletterController extends Controller
+{
+    public function store(Request $request):RedirectResponse{
+        $incomingFields = $request->validate(
+            [
+                'email'=>['required', 'email']
+            ],
+            [
+            'email'=>'Email-ul tău este neccesar!',
+            ],
+    );
+        Newsletter::create([
+            'email'=>$incomingFields['email']
+        ],
+    
+);
+
+        return redirect()->back()->with(['success'=>"Email-ul tău este conectat cu success la newsletter!"]);
+    }
+}
