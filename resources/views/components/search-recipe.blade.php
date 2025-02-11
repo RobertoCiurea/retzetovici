e9bf79015f621a9ce28ba269a7e6835cd09c92e5
RobertoCiurea <robertociureaionut@gmail.com>
2025-02-11 20:40:06 +0200

    added admin search recipe feature (to be implemented)

---
+++ resources/views/components/search-recipe.blade.php
@@ -0,0 +1,8 @@
+
+    <form action="" method="POST" class="flex flex-col gap-5">
+        <h1 class="font-kanit font-semibold text-lg md:text-xl">Cautare rețetă</h1>
+        <input type="text"  placeholder="ID rețetă" name="recipeId" class="bg-transparent border-b-2 border-accent focus:border-none focus:outline-2 focus:outline-accentLight px-2 focus:rounded-md"
+        onkeypress="return (event.charCode !=8 && event.charCode ==0 || (event.charCode >= 48 && event.charCode <= 57))" 
+        >
+        <button class="rounded-lg text-white shadow-xl bg-green-700 hover:bg-green-800 transition-colors px-2 py-1">Cautare</button>
+    </form>
