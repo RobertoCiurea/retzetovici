b1ce460154d0f64cb4a99a5383a8f70740cfc81d
RobertoCiurea <robertociureaionut@gmail.com>
2025-02-11 20:30:32 +0200

    added admin panel page

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <div class="flex flex-col items-center gap-20">
+        <x-admin-panel title="Admin Panel"></x-admin-panel>
+        <x-search-recipe></x-search-recipe>
+    </div>
+</x-layout>
