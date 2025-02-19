caca0c34789106f96aae95e375fb03eb2560aaf9
RobertoCiurea <robertociureaionut@gmail.com>
2025-02-19 18:28:53 +0200

    added logic for opening/closing modal

---
+++ resources/js/modal.js
@@ -0,0 +1,10 @@
+const modal = document.querySelector("[data-modal]");
+const openButton = document.querySelector("[data-open-modal]");
+const closeButton = document.querySelector("[data-close-modal]");
+
+openButton.addEventListener('click', () => {
+    modal.showModal();
+})
+closeButton.addEventListener('click', () => {
+    modal.close();
+})
