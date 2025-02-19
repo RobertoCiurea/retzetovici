79f99ac79b57e743e3ee2bf2c49b280cafccf469
RobertoCiurea <robertociureaionut@gmail.com>
2025-02-19 18:29:55 +0200

    implemented search recipe logic and added error feedback

--- resources/views/components/search-recipe.blade.php
+++ resources/views/components/search-recipe.blade.php
@@ -1,6 +1,10 @@
 
-    <form action="" method="POST" class="flex flex-col gap-5">
+    <form action="{{route('recipes.recipe.search')}}" method="POST" class="flex flex-col gap-5">
+        @csrf
         <h1 class="font-kanit font-semibold text-lg md:text-xl">Cautare rețetă</h1>
+        @error('recipeId')
+            <span class="text-red-600 font-kanit">{{$message}}</span>
+        @enderror
         <input type="text"  placeholder="ID rețetă" name="recipeId" class="bg-transparent border-b-2 border-accent focus:border-none focus:outline-2 focus:outline-accentLight px-2 focus:rounded-md"
         onkeypress="return (event.charCode !=8 && event.charCode ==0 || (event.charCode >= 48 && event.charCode <= 57))" 
         >
