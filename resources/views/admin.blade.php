57f8bf2dde798357d0ddcdd3bf63dc472df653e1
RobertoCiurea <robertociureaionut@gmail.com>
2025-03-17 17:32:30 +0200

    added admin navigation button for user's feedback messages

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,6 +1,7 @@
 <x-layout>
     <div class="flex flex-col items-center gap-20">
         <x-admin-panel title="Admin Panel"></x-admin-panel>
-        <x-search-recipe></x-search-recipe>
+        <x-search-recipe/>
+        <x-admin-buttons/>
     </div>
 </x-layout>
