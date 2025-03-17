431f959ed5b9ec24ce06e15983f64b389f7e601a
RobertoCiurea <robertociureaionut@gmail.com>
2025-03-17 17:33:13 +0200

    added navigation admin buttons for user's feedback messages

---
+++ resources/views/components/admin-buttons.blade.php
@@ -0,0 +1,4 @@
+<div class="flex flex-col md:flex-row w-full justify-center gap-10">
+    <a href="/messages/contact"><button type="button" class="px-2 py-1 md:text-lg bg-accentLight hover:bg-accent transition-colors rounded-md text-white shadow-xl">Mesaje Contact</button></a>
+    <a href="/messages/report"><button type="button" class="px-2 py-1 md:text-lg bg-accentLight hover:bg-accent transition-colors rounded-md text-white shadow-xl">Mesaje Report</button></a>
+</div>
