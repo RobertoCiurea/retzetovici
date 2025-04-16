5ae251c7db854d8f52077f72bd09128d6e142eef
RobertoCiurea <robertociureaionut@gmail.com>
2025-04-16 20:59:53 +0300

    modify modal buttons to adapt for any number of modals on the same page

--- resources/js/modal.js
+++ resources/js/modal.js
@@ -1,10 +1,17 @@
-const modal = document.querySelector("[data-modal]");
-const openButton = document.querySelector("[data-open-modal]");
-const closeButton = document.querySelector("[data-close-modal]");
+document.querySelectorAll('[data-open-modal]').forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.nextElementSibling;
+        if (modal) {
+            modal.showModal();
+        }
+    });
+});
 
-openButton.addEventListener('click', () => {
-    modal.showModal();
-})
-closeButton.addEventListener('click', () => {
-    modal.close();
-})
+document.querySelectorAll('[data-close-modal]').forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('dialog');
+        if (modal) {
+            modal.close();
+        }
+    });
+});
