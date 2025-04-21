2b19ee4cd80706a27377be9ce2d84a83abf56e34
RobertoCiurea <robertociureaionut@gmail.com>
2025-04-21 17:19:37 +0300

    added description meta tag

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,10 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-
+        <meta
+            name="description"
+            content="Retzetovici: Blogul tau preferat de retete culinare!"
+        />
         <title>Retzetovici</title>
         @vite(["resources/css/app.css", "resources/js/app.js"])
         @stack("scripts")
