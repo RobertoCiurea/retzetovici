2db00edf6e00c60b4fdc8ae167340cf47f8c4425
RobertoCiurea <robertociureaionut@gmail.com>
2025-04-21 14:12:09 +0300

    added admin side report page

---
+++ resources/views/admin-reports.blade.php
@@ -0,0 +1,89 @@
+@php
+    $selectedContent = request()->get("content");
+@endphp
+
+<x-layout>
+    <div class="flex flex-col items-center pt-20 md:px-10">
+        <h1 class="font-quicksand text-xl text-black md:text-3xl">
+            Mesaje report
+        </h1>
+        @session("success")
+            <span class="text-sm text-green-700 md:text-base">
+                {{ session("success") }}
+            </span>
+        @endsession
+
+        @session("error")
+            <span class="text-sm text-red-600 md:text-base">
+                {{ session("error") }}
+            </span>
+        @endsession
+
+        <section class="mt-20 flex w-full justify-around font-mono text-black">
+            <form action="" method="GET" class="group flex flex-col gap-1">
+                <button type="submit" class="md:text-xl">
+                    Report-uri nerezolvate
+                </button>
+                <input type="hidden" value="unresolved" name="content" />
+                @if (request("content") === "unresolved")
+                    <span class="block h-1 w-full rounded-md bg-black"></span>
+                @else
+                    <span
+                        class="block h-1 w-0 rounded-md bg-black transition-all duration-500 group-hover:w-full"
+                    ></span>
+                @endif
+            </form>
+
+            <form action="" method="GET" class="group flex flex-col gap-1">
+                <button type="submit" class="md:text-xl">
+                    Report-uri rezolvate
+                </button>
+                <input type="hidden" value="resolved" name="content" />
+                @if (request("content") === "resolved")
+                    <span class="block h-1 w-full rounded-md bg-black"></span>
+                @else
+                    <span
+                        class="block h-1 w-0 rounded-md bg-black transition-all duration-500 group-hover:w-full"
+                    ></span>
+                @endif
+            </form>
+
+            <form
+                action="/messages/report"
+                method="GET"
+                class="group flex flex-col gap-1"
+            >
+                <input type="hidden" value="in-process" name="content" />
+                <button type="submit" class="md:text-xl">
+                    Report-uri în curs de rezolvare
+                </button>
+                @if (request("content") === "in-process")
+                    <span class="block h-1 w-full rounded-md bg-black"></span>
+                @else
+                    <span
+                        class="block h-1 w-0 rounded-md bg-black transition-all duration-500 group-hover:w-full"
+                    ></span>
+                @endif
+            </form>
+        </section>
+    </div>
+    <div class="mt-20 flex w-full justify-center">
+        <div
+            class="nd:grid-cols-2 grid grid-cols-1 gap-5 md:gap-20 lg:grid-cols-3"
+        >
+            @foreach ($reports as $report)
+                @if (! $selectedContent || strtolower($report->status) === $selectedContent)
+                    <x-report-card
+                        :id="$report->id"
+                        :title="$report->title"
+                        :name="$report->name"
+                        :email="$report->email"
+                        :date="$report->created_at->format('d-m-20-y')"
+                        :status="$report->status"
+                        :image="$report->image"
+                    />
+                @endif
+            @endforeach
+        </div>
+    </div>
+</x-layout>
