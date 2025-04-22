62e1f837b5eb9b8fc6ecd32218c3fa65d19f4a8d
RobertoCiurea <robertociureaionut@gmail.com>
2025-04-22 16:48:27 +0300

    filters for /my-account route

--- resources/views/components/recipes-grid.blade.php
+++ resources/views/components/recipes-grid.blade.php
@@ -1,27 +1,35 @@
-<div class="flex flex-col gap-10 items-center font-kanit pt-10">
-    <h1 class="text-xl md:text-2xl lg:text-3xl text-black font-semibold">{{$title}}</h1>
-    <x-filters/>
-    @if (!$recipes->isEmpty())
-        <div class="grid grid-cols-1 lg:grid-cols-2 gap-5 lg:gap-10 xl:grid-cols-3 ">
-            
-        @foreach ($recipes as $recipe)
-        <x-recipe-card
-        :id="$recipe->id"
-        :image="$recipe->image"
-            :title="$recipe->title" 
-            :category="$recipe->category"
-            :difficulty="$recipe->difficulty"
-            :duration="$recipe->duration"
-            :likes="$recipe->likes"
-            :views="$recipe->views"
-            :username="$recipe->username"
-            :date="$recipe->created_at->format('d m 20y')"
-            :ingredients="$recipe->ingredients"
-            :tags="$recipe->tags"
-            />
+<div class="flex flex-col items-center gap-10 pt-10 font-kanit">
+    <h1 class="text-xl font-semibold text-black md:text-2xl lg:text-3xl">
+        {{ $title }}
+    </h1>
+    @if (Route::currentRouteName() !== "my-account")
+        <x-filters />
+    @endif
+
+    @if (! $recipes->isEmpty())
+        <div
+            class="grid grid-cols-1 gap-5 lg:grid-cols-2 lg:gap-10 xl:grid-cols-3"
+        >
+            @foreach ($recipes as $recipe)
+                <x-recipe-card
+                    :id="$recipe->id"
+                    :image="$recipe->image"
+                    :title="$recipe->title"
+                    :category="$recipe->category"
+                    :difficulty="$recipe->difficulty"
+                    :duration="$recipe->duration"
+                    :likes="$recipe->likes"
+                    :views="$recipe->views"
+                    :username="$recipe->username"
+                    :date="$recipe->created_at->format('d m 20y')"
+                    :ingredients="$recipe->ingredients"
+                    :tags="$recipe->tags"
+                />
             @endforeach
         </div>
-        @else
-            <span class="text-lg md:text-xl lg:text-2xl text-red-600 font-semibold">Nu există rețete</span>
+    @else
+        <span class="text-lg font-semibold text-red-600 md:text-xl lg:text-2xl">
+            Nu există rețete
+        </span>
     @endif
 </div>
